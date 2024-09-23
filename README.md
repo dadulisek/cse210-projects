79c9c817af83ffe76ce8fb57143a40022e601724
dadulisek <navratildada@gmail.com>
2024-09-23 23:40:03 +0200

    Week 02 learning

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cse210-ww-student-template
 
-David Navratil
+dadulisek
 
 This is the starter code for students in the worldwide content version of CSE 210.
 
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        job1._jobTitle = "Intern";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+        job1._company = "Apple";
+
+        job2._jobTitle = "Janitor";
+        job2._startYear = 2015;
+        job2._endYear = 2020;
+        job2._company = "Mikerowesoft";
+
+        Resume resume1 = new Resume();
+        resume1._name = "Lee Gump";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
