c5862ebddb8797574e5018201d4a2265ea75160e
dadulisek <navratildada@gmail.com>
2024-09-27 16:09:04 +0200

    Week 02 prove save 1

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public string _date;
+    public string _promoptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        string anwser;
+
+        do
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+
+            anwser = Console.ReadLine();
+
+            if (anwser == "1")
+            {
+
+            }
+            else if (anwser == "2")
+            {
+
+            }
+            else if (anwser == "3")
+            {
+
+            }
+            else if (anwser == "4")
+            {
+
+            }
+        } while (anwser != "5");
     }
 }
