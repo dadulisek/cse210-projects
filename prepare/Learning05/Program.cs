215db1b961d9dcdbead3fd09c615488be5ebb226
dadulisek <navratildada@gmail.com>
2024-10-14 12:28:29 +0200

    Week 05 prepare done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Peter Parker", "Fractions", "7.3", "18-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Winters", "European History", "Cause of World War 2");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
