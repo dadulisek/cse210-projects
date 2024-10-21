e12aeca54786024a6b14e2437e60a6bc3269b98f
dadulisek <navratildada@gmail.com>
2024-10-21 14:25:25 +0200

    Week 06 Develop set up

---
+++ prove/Develop06/GoalManager.cs
@@ -0,0 +1,51 @@
+public class GoalManager
+{
+    private int _score;
+    private List<Goal> _goals;
+
+    public GoalManager()
+    {
+        _score = 0;
+        _goals = new List<Goal>();
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+    public void LoadGoals()
+    {
+
+    }
+}
